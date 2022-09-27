6bb51fd593bb1b1811f0f435c608d5228a0d0435
cwzchan <cwchan0212@gmail.com>
2022-09-27 00:42:03 +0000

    Update Statistics.js

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -14,11 +14,11 @@ const Statistics = ({ labels, data, base, symbol }) => {
             Min: {sorted[0][1].toFixed(4)} {symbol}/{base} on {sorted[0][0]}
           </span>
           <span>
-            Max: {sorted[sorted.length - 1][1].toFixed(4)} {symbol}/{base} on
+            Max: {sorted[sorted.length - 1][1].toFixed(4)} {symbol}/{base} on{" "}
             {sorted[sorted.length - 1][0]}
           </span>
           <span className="red">
-            Now: {data[data.length - 1].toFixed(4)} {symbol}/{base} on
+            Now: {data[data.length - 1].toFixed(4)} {symbol}/{base} on{" "}
             {labels[labels.length - 1]}
           </span>
         </div>
